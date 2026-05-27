4a18bfe937d9e58430fb1559740a983a5eb57500
Universal Conquistador <universalconquistador@gmail.com>
2026-05-27 14:23:21 -0700

    TEMP: Adds model structures from Penumbra for use with IDA.

--- FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/ModelResourceHandle.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/ModelResourceHandle.cs
@@ -29,3 +29,94 @@ public unsafe partial struct ModelResourceHandle {
     [MemberFunction("E8 ?? ?? ?? ?? 84 C0 75 12 B0 F6")]
     public partial bool LoadMaterials();
 }
+
+[StructLayout(LayoutKind.Explicit, Size = 0x44)]
+public unsafe partial struct ModelFileHeader {
+    [FieldOffset(0x00)] public uint Version;
+    [FieldOffset(0x04)] public uint StackSize; // Size of all the vertex declarations
+    [FieldOffset(0x08)] public uint RuntimeSize;
+    [FieldOffset(0x0C)] public ushort VertexDeclarationCount;
+    [FieldOffset(0x0E)] public ushort MaterialCount;
+    [FieldOffset(0x10)] internal FixedSizeArray3<uint> _vertexOffset;
+    [FieldOffset(0x1C)] internal FixedSizeArray3<uint> _indexOffset;
+    [FieldOffset(0x28)] internal FixedSizeArray3<uint> _vertexBufferSize;
+    [FieldOffset(0x34)] public uint IndexBufferSize;
+    [FieldOffset(0x40)] public byte LodCount;
+    [FieldOffset(0x41)] public bool EnableIndexBufferStreaming;
+    [FieldOffset(0x42)] public bool EnableEdgeGeometry;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x9)]
+public unsafe struct ModelStringData {
+    [FieldOffset(0x00)] public uint StringCount;
+    [FieldOffset(0x04)] public uint DataLength;
+    [FieldOffset(0x08)] public fixed byte Data[1];
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+public unsafe partial struct ModelVertexElement {
+    [FieldOffset(0x00)] public byte Stream;
+    [FieldOffset(0x01)] public byte Offset;
+    [FieldOffset(0x02)] public byte Type;
+    [FieldOffset(0x03)] public byte Usage;
+    [FieldOffset(0x04)] public byte UsageIndex;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x08 * 17)]
+public unsafe partial struct ModelVertexDeclaration {
+    [FieldOffset(0x00)] private FixedSizeArray17<ModelVertexElement> _vertexElements;
+}
+
+[Flags]
+public enum ModelFlags1 : byte {
+    DustOcclusionEnabled = 0x80,
+    SnowOcclusionEnabled = 0x40,
+    RainOcclusionEnabled = 0x20,
+    Unknown1 = 0x10,
+    LightingReflectionEnabled = 0x08,
+    WavingAnimationDisabled = 0x04,
+    LightShadowDisabled = 0x02,
+    ShadowDisabled = 0x01,
+}
+
+[Flags]
+public enum ModelFlags2 : byte {
+    Unknown2 = 0x80,
+    BgUvScrollEnabled = 0x40,
+    EnableForceNonResident = 0x20,
+    ExtraLodEnabled = 0x10,
+    ShadowMaskEnabled = 0x08,
+    ForceLodRangeEnabled = 0x04,
+    EdgeGeometryEnabled = 0x02,
+    Unknown3 = 0x01,
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x38)]
+public unsafe partial struct ModelMeshHeader {
+    [FieldOffset(0x00)] public float Radius;
+    [FieldOffset(0x04)] public ushort MeshCount;
+    [FieldOffset(0x06)] public ushort AttributeCount;
+    [FieldOffset(0x08)] public ushort SubmeshCount;
+    [FieldOffset(0x0A)] public ushort MaterialCount;
+    [FieldOffset(0x0C)] public ushort BoneCount;
+    [FieldOffset(0x0E)] public ushort BoneTableCount;
+    [FieldOffset(0x10)] public ushort ShapeCount;
+    [FieldOffset(0x12)] public ushort ShapeMeshCount;
+    [FieldOffset(0x14)] public ushort ShapeValueCount;
+    [FieldOffset(0x16)] public byte LodCount;
+    [FieldOffset(0x17)] public ModelFlags1 Flags1;
+    [FieldOffset(0x18)] public ushort ElementIdCount;
+    [FieldOffset(0x1A)] public byte TerrainShadowMeshCount;
+    [FieldOffset(0x1B)] public ModelFlags2 Flags2;
+    [FieldOffset(0x1C)] public float ModelClipOutDistance;
+    [FieldOffset(0x20)] public float ShadowClipOutDistance;
+    [FieldOffset(0x24)] public ushort CullingGridCount;
+    [FieldOffset(0x26)] public ushort TerrainShadowSubmeshCount;
+    [FieldOffset(0x28)] public byte Flags3;
+    [FieldOffset(0x29)] public byte BGChangeMaterialIndex;
+    [FieldOffset(0x2A)] public byte BGCrestChangeMaterialIndex;
+    [FieldOffset(0x2B)] public byte NeckMorphCount;
+    [FieldOffset(0x2C)] public ushort BoneTableArrayCountTotal;
+    [FieldOffset(0x2E)] public ushort Unknown8;
+    [FieldOffset(0x30)] public ushort Unknown9;
+}
